0998b9e3bbabbf15aa176b7a0921df1c3d6122eb
matt.snider <matt.snider@gmail.com>
2012-07-09 19:07:41 -0400

    built database helper and first fragment for main view.

--- UltitrackPro/src/com/appsmarttech/utpro/main_activity.java
+++ UltitrackPro/src/com/appsmarttech/utpro/main_activity.java
@@ -1,17 +1,34 @@
 package com.appsmarttech.utpro;
 
+import java.io.IOException;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NavUtils;
 
-public class main_activity extends Activity {
-
+public class main_activity extends FragmentActivity {
+	
+	//Declarations
+	DBHelper_activity db;
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("UltiTrack Pro");
+        db = new DBHelper_activity(this);
+        try {
+			db.createDataBase();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
     }
 
     @Override
