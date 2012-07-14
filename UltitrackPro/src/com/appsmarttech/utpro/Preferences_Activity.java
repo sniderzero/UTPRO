187d3db93cfdf1c61646be3271e472742a3124fe
matt.snider <matt.snider@gmail.com>
2012-07-14 13:25:06 -0400

    got the action bar and cab working on the program fragment :)

---
+++ UltitrackPro/src/com/appsmarttech/utpro/Preferences_Activity.java
@@ -0,0 +1,7 @@
+package com.appsmarttech.utpro;
+
+import android.preference.PreferenceActivity;
+
+public class Preferences_Activity extends PreferenceActivity{
+
+}
--- UltitrackPro/src/com/appsmarttech/utpro/Programs_Fragment.java
+++ UltitrackPro/src/com/appsmarttech/utpro/Programs_Fragment.java
@@ -1,17 +1,26 @@
 package com.appsmarttech.utpro;
 
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.ActionMode;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -23,6 +32,12 @@ public class Programs_Fragment extends SherlockFragment {
 		SQLiteDatabase db;
 		ListView lvPrograms;
 		Cursor cPrograms;
+		SharedPreferences spPreferences;
+		Editor ePreferences;
+		String sActiveProgram;
+		Integer iActiveProgram, iProgramID;
+		ActionMode mActionMode;
+		ActionBar actionBar;
 		
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, 
@@ -30,12 +45,42 @@ public class Programs_Fragment extends SherlockFragment {
     	// Inflate the layout for this fragment
    	 	View vPrograms = inflater.inflate(R.layout.programs_fragment, container, false);
    	 	
+        //open preferences
+        spPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        //declare preference editor
+        ePreferences = spPreferences.edit();
+
+        //grabbing the active program from preferences
+        sActiveProgram = spPreferences.getString("kActiveProgram", "0");
+        
+        //converting it to Integer
+        iActiveProgram = Integer.valueOf(sActiveProgram);
+        
+        //changing action bar settings
+        actionBar = getSherlockActivity().getSupportActionBar();
+        
+        actionBar.setHomeButtonEnabled(true);
+        
+        
+/*
+        //converting back to a string
+        strRuns = String.valueOf(intRuns);
+        //storing in preferences
+        edit.putString("runnumber", strRuns);
+        edit.commit(); 
+        */
+   	 	
    	 	//telling it that it has an actionbar
    	 	setHasOptionsMenu(true);
    	 	
+   	 	
+   	 	//setting activity title
+   	 	getActivity().setTitle("Select Program");
+   	 	
    	 	//assigning listview to listview widget
    	 	lvPrograms = (ListView)vPrograms.findViewById(R.id.lvPrograms);
-        
+   	 	
         //opening database
         db = (new DBHelper_activity(getActivity())).getReadableDatabase();
         
@@ -53,6 +98,7 @@ public class Programs_Fragment extends SherlockFragment {
 	    private Cursor mCursor;  
 	    private Context mContext;  
 	    LayoutInflater mInflater;  
+	    
 	  
 	    public adapter(Context context, Cursor cPrograms) {  
 	      super(context, cPrograms, true);  
@@ -61,18 +107,60 @@ public class Programs_Fragment extends SherlockFragment {
 	    }  
 	  
 	    @Override  
-	    public void bindView(View view, Context context, Cursor cursor) {  
-	      TextView tvProgramName = (TextView)view.findViewById(R.id.tvProgramName);  
+	    public void bindView(View view, Context context, final Cursor cursor) {  
+	      TextView tvProgramName = (TextView)view.findViewById(R.id.tvProgramName);
+	      CheckBox cbProgram = (CheckBox)view.findViewById(R.id.cbProgram);
 	      tvProgramName.setText(cursor.getString(cursor.getColumnIndex("trackName")));
 	  
- 
-	      /*
-	      if(cursor.isNull(7)){
-	    	  ((ImageView)view.findViewById(R.id.imgCheck)).setVisibility(View.INVISIBLE);
+	      //retrieving the program ID from the cursor
+	      iProgramID = cursor.getInt(cursor.getColumnIndex("_id"));
+	      //if programID is equal to the active program
+	      if(iProgramID == iActiveProgram){
+	    	  //then show the active textview
+	    	  ((TextView)view.findViewById(R.id.tvActive)).setVisibility(View.VISIBLE);
 	      }
 	      else{
-	    	  ((ImageView)view.findViewById(R.id.imgCheck)).setVisibility(View.VISIBLE);
-	      } */
+	    	  //if not hide the active textview
+	    	  ((TextView)view.findViewById(R.id.tvActive)).setVisibility(View.INVISIBLE);
+	      } 
+	      //building an onclick listener for tvProgramName
+	      OnClickListener tvProgramListener = new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				
+				Toast.makeText(getActivity(), cursor.getString(cursor.getColumnIndex("trackName")), Toast.LENGTH_SHORT)
+				.show();
+				
+		        //setting active program for testing purposes
+		        //ePreferences.putString("kActiveProgram", cursor.getInt(cursor.getColumnIndex("_id")));
+		        //ePreferences.commit();
+				
+			}
+	    	  
+	      };
+	      //assigning the onclicklistener to the tvProgramName text view
+	      tvProgramName.setOnClickListener(tvProgramListener);
+	      
+	      //building an onclick listener for cbProgram
+	      OnCheckedChangeListener cbProgramListener = new OnCheckedChangeListener(){
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				
+				mActionMode = getSherlockActivity().startActionMode(new mActionModeCallback());
+				
+				Toast.makeText(getActivity(), "check status changed" , Toast.LENGTH_SHORT)
+				.show();
+				
+			}
+	    	  
+	      };
+	      
+	      cbProgram.setOnCheckedChangeListener(cbProgramListener);
+	      
+	      
+	      
 	      }  
 	  
 	    @Override  
@@ -81,6 +169,48 @@ public class Programs_Fragment extends SherlockFragment {
 	      return view;  
 	    }  
 	  }  
+	
+		//building contextual actionbar stuff
+	
+	private final class mActionModeCallback implements ActionMode.Callback {
+
+		// Called when the action mode is created; startActionMode() was called
+		public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+			// Inflate a menu resource providing context menu items
+			MenuInflater inflater = mode.getMenuInflater();
+			// Assumes that you have "contexual.xml" menu resources
+			inflater.inflate(R.menu.programs_contextual_actionbar, menu);
+			mode.setTitle("1 Giant ball sack");
+			return true;
+		}
+
+		// Called each time the action mode is shown. Always called after
+		// onCreateActionMode, but
+		// may be called multiple times if the mode is invalidated.
+		public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+			return false; // Return false if nothing is done
+		}
+
+		// Called when the user selects a contextual menu item
+		public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+			switch (item.getItemId()) {
+			case R.id.miEdit:
+				Toast.makeText(getActivity(), "Selected menu",
+						Toast.LENGTH_LONG).show();
+				mode.finish(); // Action picked, so close the CAB
+				return true;
+			default:
+				return false;
+			}
+		}
+
+		// Called when the user exits the action mode
+		public void onDestroyActionMode(ActionMode mode) {
+			mActionMode = null;
+		}
+	};
+	
+	
 	   //creating the options menu - actionbar
 		public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
 			inflater.inflate(R.menu.customize_actionbar, menu);
