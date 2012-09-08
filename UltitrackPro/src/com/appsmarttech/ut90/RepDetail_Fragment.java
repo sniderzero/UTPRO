107aa874d2276ff0b574b2c151d17f89f037e535
matt.snider <matt.snider@gmail.com>
2012-09-08 18:57:46 -0400

    database update .... exercises are in order.

--- UltitrackPro/src/com/appsmarttech/ut90/RepDetail_Fragment.java
+++ UltitrackPro/src/com/appsmarttech/ut90/RepDetail_Fragment.java
@@ -1,6 +1,5 @@
 package com.appsmarttech.ut90;
 
-import java.util.Calendar;
 import java.util.List;
 import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.view.Menu;
@@ -9,11 +8,11 @@ import com.actionbarsherlock.view.MenuItem;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -46,6 +45,7 @@ public class RepDetail_Fragment extends SherlockFragment{
 	SharedPreferences spPreferences;
 	Spinner spBands;
 	SpinnerAdapter spBandsAdapter;
+	Intent inDays;
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, 
@@ -425,6 +425,8 @@ public class RepDetail_Fragment extends SherlockFragment{
 						repNavListener.mNavigation(1);
 						break;
 					default:
+						inDays = new Intent(getActivity(), Days_Activity.class);
+						startActivity(inDays);
 						break;
 					}
 
